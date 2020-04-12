5053587f574466e83e42cc04a71064f38d1bc1ac
Bartek K <kontener00@gmail.com>
2020-04-12 17:37:24 +0200

    script.js update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-function printMessage(msg){
+function printMessage(msg); {
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
+
+	let computerMove = `kamień`;
+	let playerMove = `papier`;
+	printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+	printMessage('Zagrałem' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
 }
 
 function clearMessages(){
