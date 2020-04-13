216c13cfe70cc82ce2cedf9743f8201cb641738d
Bartek K <kontener00@gmail.com>
2020-04-13 14:51:11 +0200

    script.js update - add two else if var.

--- js/script.js
+++ js/script.js
@@ -6,6 +6,16 @@ let computerMove = 'nieznany ruch';
 
 if (randomNumber == 1) {
     computerMove = 'kamień';
+
+else if (randomNumber == 2) {
+        computerMove = 'papier'
+    }
+
+    else if (randomNumber == 3) {
+        computerMove = 'nożyce'
+    }
+
 }
+
 
 printMessage('Mój ruch to: ' + computerMove);
