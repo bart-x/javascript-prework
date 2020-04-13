6d4c00ecf569f7da9d9f74a3c63b12432e63d9b2
Bartek K <kontener00@gmail.com>
2020-04-13 15:12:43 +0200

    script.js update - multiple choice var.

--- js/script.js
+++ js/script.js
@@ -17,5 +17,60 @@ else if (randomNumber == 2) {
 
 }
 
-
 printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if (playerInput == '1') {
+    playerMove = 'kamień';
+
+  else if (playerInput == '2') {
+        playerMove = 'papier'
+    }
+
+    else if (playerInput == '3') {
+        playerMove = 'nożyce'
+    }
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+if (computerMove == 'kamień' && playerMove == 'papier') {
+    printMessage('Ty wygrywasz!');
+}
+
+else if (playerInput < 3) {
+    printMessage('nieznany ruch')
+}
+
+else (computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis')
+}
+
+if (computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('Ty wygrywasz!');
+}
+
+else if (playerInput < 3) {
+    printMessage('nieznany ruch')
+}
+
+else (computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis')
+}
+
+if (computerMove == 'nożyce' && playerMove == 'papier') {
+    printMessage('Przegrałeś!');
+}
+
+else if (playerInput < 3) {
+    printMessage('nieznany ruch')
+}
+
+else (computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis')
+}
